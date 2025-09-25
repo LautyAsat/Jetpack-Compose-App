8965b080eae3c99581ef6ff9f444004997d61016
Lautaro Asat <lautaronorielasat@gmail.com>
2025-09-24 23:05:16 -0300

    Add manual operator

--- app/src/main/java/com/example/allengineeringinone/ui/common/Chat/data/repository/ChatRepository.kt
+++ app/src/main/java/com/example/allengineeringinone/ui/common/Chat/data/repository/ChatRepository.kt
@@ -1,5 +1,10 @@
 package com.example.allengineeringinone.ui.common.Chat.data.repository
 
+import com.example.allengineeringinone.ui.common.Chat.data.model.Message
+import kotlinx.coroutines.flow.Flow
+
 interface ChatRepository {
     suspend fun getResponsesFor(question: String): Result<String?>
+
+    suspend fun observeMessages(): Flow<String>
 }
